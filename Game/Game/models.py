922cc59e28330c72f7120cd544febc6c072391da
estela19 <e9857256@naver.com>
2020-01-17 05:32:03 +0900

    Feat(model) : Add player, food models

--- Game/Game/models.py
+++ Game/Game/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 
+class Player(models.Model):
+    objects = models.Manager()
+    name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    posX = models.FloatField
+    posY = models.FloatField
+    radius = models.FloatField
+    color = models.CharField(max_length=7)
 
+class Food(models.Model):
+    objects = models.Manager()
+    posX = models.FloatField
+    posY = models.FloatField
+    radius = models.FloatField
+    color = models.CharField(max_length=7)
--- Game/Game/views.py
+++ Game/Game/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from Game.models import Player
 
 def Login(request):
     if request.method == 'POST':
         name = request.POST['name']
-        u = User(username = name)
-        u.save()
+#        u = User(username = name)
+#        u.save()
     return render(request, 'Game/Login.html')
