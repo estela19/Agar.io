112043f7d1577125b947b9331fc24ddd994db27d
estela19 <e9857256@naver.com>
2020-01-17 08:26:54 +0900

    Fix(logic) : Fix AbsoluteToRelative method

--- Game/Information/logic.py
+++ Game/Information/logic.py
@@ -1,7 +1,11 @@
+from Game.Game.models import Player 
 import math
 
-def AbsoluteToRelative(PlayerX, PlayerY, ax, ay) :
-    return ax - PlayerX, ay - PlayerY    
+def AbsoluteToRelative(name, ax, ay) :
+    player = Player.objects.filter(name=name)
+    player.posX += ax
+    player.posY += ay
+    player.save()
 
 def RelativeToAbsolute(PlayerX, PlayerY, rx, ry) :
     return PlayerX + rx, PlayerY + ry
@@ -15,7 +19,7 @@ def Normalize(x, y) :
 
 def ChangePlayerPosition(PlayerAbsoluteX, PlayerAbsoluteY, MouseRelativeX, MouseRelativeY, dt) :
     """ Relative position of mouse to Absolute position of client """
-    MouseAbsoluteX, MouseAbsoluteY = RelativeToAbsolute(MouseRelativeX, MouseRelativeY)
+    MouseAbsoluteX, MouseAbsoluteY = RelativeToAbsolute(PlayerAbsoluteX, PlayerAbsoluteY, MouseRelativeX, MouseRelativeY)
     dx, dy = Normalize(PlayerAbsoluteX - MouseAbsoluteX, PlayerAbsoluteY - MouseAbsoluteY)
     return PlayerAbsoluteX + dx * dt, PlayerAbsoluteY + dy * dt
     
