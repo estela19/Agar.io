0a7b6f1bc57d18dbebf0971260e4e9f77fb55d5d
estela19 <e9857256@naver.com>
2020-02-02 16:44:22 +0900

    Fix(view) : fix Login post

--- Game/Game/views.py
+++ Game/Game/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from Game.models import Player
+import random
 
 def Login(request):
     if request.method == 'POST':
         name = request.POST['name']
+        X = random.randint(0,100)
+        Y = random.randint(0,100)
+        Player.objects.create(
+            name = name,
+            posX = X,
+            posY = Y,
+            radius = 0.5,
+            color = '#ffffff'
+        )
 #        u = User(username = name)
 #        u.save()
     return render(request, 'Game/Login.html')
